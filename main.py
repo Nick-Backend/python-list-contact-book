c28b97cb7cc101725b7038103518958eed9fac1a
Ozodbek Tirkashov <tirkashovo03@gmail.com>
2025-12-27 17:55:24 +0500

    bajarildi

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from typing import List
-
-
-def show_menu() -> None:
+def show_menu():
     print("\n====== 📱 Contact Book v2.2 ======")
     print("1. + Yangi kontakt qoshish")
     print("2. 📄 Barcha kontaktlarni korish")
@@ -10,32 +7,81 @@ def show_menu() -> None:
     print("5. 🚪 Dasturni yakunlash")
 
 
-def is_valid_contact(contact: List) -> bool:
-    pass
+def is_valid_contact(contact):
+    name = contact[0]
+    number = contact[1]
+    gmail = contact[2]
+
+    if name == "" or not name.isalpha():
+        return False
+
+    if number == "" or not number.isdigit() or not (9 <= len(number) <= 13):
+        return False
+
+    if gmail == "" or "@" not in gmail:
+        return False
+
+    return True
 
 
-def add_contact(contact_list: List[List]) -> None:
-    pass
+def add_contact(contacts):
+    name = input("Ismni kiriting: ").strip()
+    number = input("Nomerni  kiriting: ").strip()
+    gmail = input("Gmailni kiriting: ").strip()
+
+    contact = [name, number, gmail]
+    if not is_valid_contact(contact):
+        print("Kontak xato kiritildi! ")
+        return
+    
+    contacts.append(contact)
+
+    print("Kontak qo'shildi! ")
 
 
-def list_contacts(contact_list: List[List]) -> None:
-    pass
+def list_contacts(contacts):
+    if not contacts:
+        print("Kontak mavjud emas! ")
+        return
+    for contact in contacts:
+        print([contact[0], contact[1], contact[2]])    
+        
 
+def search_contact(contact_list):
+    if not contact_list:
+        print("Kontak mavjud emas! ")
+        return 
+    name = input("Qaysi isimni topmoqchisiz: ")
+    topildi = False
 
-def search_contact(contact_list: List[List]) -> None:
-    pass
+    for contact in contact_list:
+        if contact[0].lower() == name.lower():
+            print(contact)
+            topildi = True
+            
+    if not topildi:
+        print("Bunday isim mavjud emas! ") 
+              
+  
+def filter_gmail_contacts(contact_list):
+    topildi = False
 
+    for contact in contact_list:
+        if contact[2].endswith("@gmail.com"):
+            print(contact)
+            topildi = True
+            
 
-def filter_gmail_contacts(contact_list: List[List]) -> None:
-    pass
+    if not topildi:
+        print("Gmail topilmadi: ")
+        
 
-
-def main() -> None:
-    contacts: List[List] = []
+def main():
+    contacts = []
 
     while True:
         show_menu()
-        choice = input("Tanlov (1-5): ").strip()
+        choice = input("Tanlang (1-5): ").strip()
 
         if choice == "1":
             add_contact(contacts)
